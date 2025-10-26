e8ef8e03d203aafa7811889465e1779f594cdb5e
John Lukach <john@lukach.net>
2025-10-26 00:39:25 +0000

    html

--- auth/auth.py
+++ auth/auth.py
@@ -1,41 +1,28 @@
-import os
-
 def handler(event, context):
     
     print(event)
 
     html = """<!DOCTYPE html>
-		<html>
-  			<head>
-				<script src='https://descopecdn.com/npm/@descope/web-component@3.21.0/dist/index.js'></script>
-				<script src='https://descopecdn.com/npm/@descope/web-js-sdk@1.16.0/dist/index.umd.js'></script>
-  			</head>
-  			<body>
-				<h1>Log In With Descope Flows</h1>
-    			<p id="container"></p>
-				<script>
-					const sdk = Descope({ projectId: 'P34YxCrt7m3gXk5YJSHcwwZjyFIh', baseUrl: 'https://lunker.lukach.net', persistTokens: true, autoRefresh: true });
-					const sessionToken = sdk.getSessionToken()
-					var notValidToken
-					if (sessionToken) {
-						notValidToken = sdk.isJwtExpired(sessionToken)
-					}
-					if (!sessionToken || notValidToken) {
-						var container = document.getElementById('container');
-						container.innerHTML = '<descope-wc project-id="P34YxCrt7m3gXk5YJSHcwwZjyFIh" base-url="https://lunker.lukach.net" flow-id="sign-in"></descope-wc>';
-						const wcElement = document.getElementsByTagName("descope-wc")[0];
-						const onSuccess = (e) => {
-							sdk.refresh();
-						};
-						const onError = (err) => {
-							console.log(err);
-						};
-            			wcElement.addEventListener('success', onSuccess);
-            			wcElement.addEventListener('error', onError);
-					}
-				</script>
-  			</body>
-		</html>"""
+	<html>
+  		<head>
+			<script src="https://descopecdn.com/npm/@descope/web-component@3.21.0/dist/index.js"></script>
+			<script src="https://descopecdn.com/npm/@descope/web-js-sdk@1.16.0/dist/index.umd.js"></script>
+  		</head>
+  		<body>
+    		<p id="container"></p>
+			<descope-wc project-id="P34YxCrt7m3gXk5YJSHcwwZjyFIh" flow-id="sign-in"/>
+			<script>
+				const wcElement = document.getElementsByTagName('descope-wc')[0];
+				const onSuccess = (e) => {
+					console.log(e.detail.user.name)
+					console.log(e.detail.user.email)
+				};
+				const onError = (err) => console.log(err);
+				wcElement.addEventListener('success', onSuccess);
+				wcElement.addEventListener('error', onError);
+			</script>
+		</body>
+	</html>"""
 
     return {
         'statusCode': 200,
--- root/root.py
+++ root/root.py
@@ -1,13 +1,24 @@
-import os
-
 def handler(event, context):
     
     print(event)
 
+    html = """<!DOCTYPE html>
+	<html>
+  		<head>
+			<script src="https://descopecdn.com/npm/@descope/web-js-sdk@1.16.0/dist/index.umd.js"></script>
+  		</head>
+  		<body>
+			<script>
+                sdk.refresh()
+			</script>
+            <h1>Hello from Root!</h1>
+		</body>
+	</html>"""
+
     return {
         'statusCode': 200,
-        'body': 'Hello, World!',
+        'body': html,
         'headers': {
-            'Content-Type': 'text/plain'
+            'Content-Type': 'text/html'
         }
     }
