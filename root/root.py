7e46c8935ad3432374865349be9bb27c5989afee
John Lukach <john@lukach.net>
2025-10-27 02:00:09 +0000

    fix

--- root/root.py
+++ root/root.py
@@ -7,29 +7,30 @@ def handler(event, context):
   		<head>
 			<script src="https://descopecdn.com/npm/@descope/web-component@3.21.0/dist/index.js"></script>
 			<script src="https://descopecdn.com/npm/@descope/web-js-sdk@1.16.0/dist/index.umd.js"></script>
-			<script>
-                const sdk = Descope({ projectId: "P34YxCrt7m3gXk5YJSHcwwZjyFIh", persistTokens: true, autoRefresh: true });
-                const sessionToken = sdk.getSessionToken();
-                if (sessionToken) {
-  					if(isJwtExpired(sessionToken)) {
-						console.log('Session token has expired.');
-  					} else {
-						console.log('Session token is valid.');
-  					}                    
-                } else {
-				    const wcElement = document.getElementsByTagName('descope-wc')[0];
-				    const onSuccess = (e) => {
-					    console.log(e.detail.user.email);
-				    };
-				    const onError = (err) => console.log(err);
-				    wcElement.addEventListener('success', onSuccess);
-				    wcElement.addEventListener('error', onError);
-                }
-			</script>
         </head>
   		<body>
-    		<p id="container"></p>
-			<descope-wc project-id="P34YxCrt7m3gXk5YJSHcwwZjyFIh" flow-id="sign-in" theme="os"/>        
+    		<p id="container"></p> 
+            <script>
+                const sdk = Descope({ projectId: 'P34YxCrt7m3gXk5YJSHcwwZjyFIh', persistTokens: true, autoRefresh: true }) ;
+                const sessionToken = sdk.getSessionToken()
+                var notValidToken
+                if (sessionToken) {
+                    notValidToken = sdk.isJwtExpired(sessionToken)
+                }
+                if (!sessionToken || notValidToken) {
+                    var container = document.getElementById('container') ;
+                    container.innerHTML = '<descope-wc project-id="P34YxCrt7m3gXk5YJSHcwwZjyFIh" flow-id="sign-in"></descope-wc>';
+                    const wcElement = document.getElementsByTagName("descope-wc")[0];
+                    const onSuccess = (e) => {
+                        sdk.refresh();
+                    };
+                    const onError = (err) => {
+                        console.log(err);
+                    };
+                    wcElement.addEventListener('success', onSuccess);
+                    wcElement.addEventListener('error', onError);
+                }
+            </script>
 		</body>
 	</html>"""
 
