5d758182590917f58d52c2b7f180c926eebf314d
John Lukach <john@lukach.net>
2025-11-19 03:10:15 +0000

    redirct

--- auth/auth.py
+++ auth/auth.py
@@ -1,29 +1,35 @@
+import base64
 import os
+import requests
 
 def handler(event, context):
 
-    print(event)
-
-    print(context)
-
     if 'rawQueryString' in event and event['rawQueryString'].startswith('code='):
 
-        status = 200
-        html = '''<HTML>
-        <HEAD>
-            <TITLE>Happy Fishing!</TITLE>
-        </HEAD>
-        <BODY>
-            <CENTER>
-                <IMAGE SRC="https://lukach.net/images/lunker.png" ALT="Lunker Logo">
-                <H2>Happy Fishing!</H2>
-            </CENTER>
-        </BODY>
-        </HTML>'''
+        b64 = base64.b64encode(f"{os.environ['CLIENT_ID']}:{os.environ['CLIENT_SECRET']}".encode()).decode()
+
+        url = 'https://hello.lukach.net/oauth2/token'
+        headers = {
+            'Authorization': f'Basic {b64}',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        data = {
+            'code': event['rawQueryString'].split('=')[1],
+            'grant_type': 'authorization_code',
+            'redirect_uri': 'https://lunker.lukach.net/auth'
+        }
+
+        response = requests.post(url, headers=headers, data=data)
+
+        return {
+            'statusCode': 302,
+            'headers': {
+                'Location': f"https://lunker.lukach.net/home?token={response.json().get('access_token','')}"
+            }
+        }
  
     else:
 
-        status = 403
         html = '''<HTML>
         <HEAD>
             <TITLE>No Fishing!</TITLE>
@@ -37,7 +43,7 @@ def handler(event, context):
         </HTML>'''
 
     return {
-        'statusCode': status,
+        'statusCode': 403,
         'body': html,
         'headers': {
             'Content-Type': 'text/html'
